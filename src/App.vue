02876ccf85636b01c7a1c84d7bae81958d914668
Uriel Fisher <urielfisher2004@gmail.com>
2024-04-25 18:14:01 +0300

    app grid to flexbox

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script>
+export default{
+}
 </script>
 
 <template>
+  <div id="parent">
+  </div>
 </template>
 
 <style scoped>
+#parent {
+  /* display: grid;
+  grid-template-columns: 1fr 1fr 1fr; */
+  width: 100%;
+  height: 100%;
+}
 </style>
