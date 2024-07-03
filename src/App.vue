e870e87a742e16518cf5c438f04f41642b315dda
UrielFisher <161847890+UrielFisher@users.noreply.github.com>
2024-07-03 21:32:19 +0300

    Properly (enough) communicating with the Google Cloud Vision API (#4)
    * send request JSON with b64 image
    
    * 100% API request - response

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,60 @@
 
 export default{
   name: "App",
+  data() {
+    return {
+      json: {
+        "requests": [
+          {
+            "image": {
+              "content": ""
+            },
+            "features": [
+              {
+                "type": "TEXT_DETECTION"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    this.getImageAsBase64("./assets/google-ocr-sign.jpg")
+  },
+  methods: {
+    ocr(b64) {
+      this.json.requests[0].image.content = b64
+      fetch("https://vision.googleapis.com/v1/images:annotate",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.json)
+      })
+      .then(data=> data.json())
+      .then((data) => {console.log(data)})
+      .catch(()=>{console.log("failure");})
+    },
+    getImageAsBase64(imagePath) {
+      fetch('http://localhost:5173/src/assets/google-ocr-sign.jpg')
+        .then(response => response.blob())
+        .then(blob => {
+          var reader = new FileReader();
+          reader.onload = function() {
+            const b64 = reader.result.split(',')[1];
+            if(b64) {
+              this.ocr(b64)
+            }
+            else {console.log("Empty base64")}
+          };
+          reader.readAsDataURL(blob);
+        })
+        .catch(error => {
+          console.log("Error B64ing image: " + error)
+        });
+    },
+  },
 }
 </script>
 
